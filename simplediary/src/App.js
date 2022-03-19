9f72c7f384380db7379371e0563f6e801cdb8cad
Minju <minju1009@gmail.com>
2022-03-19 23:49:07 +0900

    add : changing state with useState

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,30 +1,6 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: '김민주',
-//     content: '하이 1',
-//     score: 3,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id: 2,
-//     author: '김보윤',
-//     content: '하이 2',
-//     score: 4,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id: 3,
-//     author: '윤서영',
-//     content: '하이 3',
-//     score: 2,
-//     created_date: new Date().getTime()
-//   }
-// ];
 
 function App() {
   const [data, setData] = useState([]);
@@ -40,18 +16,27 @@ function App() {
       created_date,
       id: dataId.current
     };
-    dataId.current++;
+    dataId.current += 1;
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
-    setData(data.filter((item) => item.id !== targetId));
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
   };
 
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onDelete={onDelete} />
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
 }
